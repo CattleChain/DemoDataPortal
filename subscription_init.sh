363cb2fbfe4044a06f8d202fd2e55890ecc5405e
Har Preet Singh <singh.1@campus.tu-berlin.de>
2020-04-27 12:09:13 +0200

    subscription

---
+++ subscription_init.sh
@@ -0,0 +1,54 @@
+
+#!/bin/bash
+
+
+echo 'adding quantumleap subscription.....'
+curl --location --request POST 'http://localhost:1026/v2/subscriptions/' \
+--header 'content-type: application/json' \
+--header 'fiware-service: cattlechain' \
+--header 'fiware-servicepath: /CattleChainService' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "description": "Notify to quantumleap",
+  "subject": {
+    "entities": [
+      {
+        "idPattern": "Event.*"
+      }
+    ],
+     "condition": {
+      "attrs": [ "activity","temprature","weight","drinkingbehaviour","rest_time","dairy_time" ]
+    }
+  },
+  "notification": {
+    "http": {
+      "url": "http://quantumleap:8668/v2/notify"
+    }
+  }
+}'
+echo 'quantumleap subscription added !!!!'
+
+
+echo 'adding cygnus subscription.....'
+curl --location --request POST 'http://localhost:1026/v2/subscriptions/' \
+--header 'content-type: application/json' \
+--header 'fiware-service: cattlechain' \
+--header 'fiware-servicepath: /CattleChainService' \
+--header 'Content-Type: application/json' \
+--data-raw '{
+  "description": "Notify to cygnus",
+  "subject": {
+    "entities": [
+      {
+        "idPattern": ".*"
+      }
+    ]
+  },
+  "notification": {
+    "http": {
+      "url": "http://cygnus:5051/notify"
+    }
+  }
+}'
+
+echo 'quantumleap cygnus added !!!!'
